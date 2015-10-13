5ec666417e5e2c8f41242e537b81876d470d6db0
Diego Castaño Chillarón <diego.castano.chillaron@alumnos.upm.es>
2015-10-13 04:51:05 +0200

    Fix FoundationToTableauController inheritance.

--- src/main/java/es/diego/castano/klondike/controllers/actions/FoundationToTableauController.java
+++ src/main/java/es/diego/castano/klondike/controllers/actions/FoundationToTableauController.java
@@ -2,18 +2,17 @@ package es.diego.castano.klondike.controllers.actions;
 
 import java.util.ArrayList;
 
-import es.diego.castano.klondike.controllers.Controller;
 import es.diego.castano.klondike.models.cards.Card;
 import es.diego.castano.klondike.models.cardstacks.Deck;
 import es.diego.castano.klondike.models.cardstacks.Foundation;
 import es.diego.castano.klondike.models.cardstacks.Tableau;
 import es.diego.castano.klondike.models.cardstacks.Waste;
 
-public class FoundationToTableauController extends Controller {
-	
+public class FoundationToTableauController extends MoveController {
+
 	private int foundationIndex = 0;
 	private int tableauIndex = 0;
-	
+
 	public FoundationToTableauController(Deck deck, Waste waste, ArrayList<Foundation> foundations,
 			ArrayList<Tableau> tableaus) {
 		super(deck, waste, foundations, tableaus);
@@ -32,6 +31,7 @@ public class FoundationToTableauController extends Controller {
 		return tableaus.get(tableauIndex).isValidOnTop(card);
 	}
 
+	@Override
 	public void move() {
 		Card topCard = foundations.get(foundationIndex).takeCardFromTop();
 		tableaus.get(tableauIndex).putCardOnTop(topCard);
